a6ffa23eabd3e101244d5dfee4073986ea729a10
miyamok <58558301+miyamok@users.noreply.github.com>
2024-02-21 22:31:08 +0900

    minor change for the case the specified person isn't in the nice person's list.

--- client/index.js
+++ client/index.js
@@ -10,9 +10,13 @@ async function main() {
     console.log("Usage: node client/index <Person's name>.\nBe careful in case the name contains a special charactor such as space!!\nExample: % node client/index \"Raymond Dickens\"");
   } else {
     const name = process.argv[2];
+    if (niceList.findIndex(n => n === name) === -1) {
+      console.log("client: the given person is not nice!!");
+    }
+    const proof =  merkleTree.getProof(niceList.findIndex(n => n === name));
     const { data: gift } = await axios.post(`${serverUrl}/gift`, {
       name: name,
-      proof: merkleTree.getProof(niceList.findIndex(n => n === name))
+      proof: proof
     });
 
     console.log({ gift });
